3efc24a913b46c91628f4b5f069cb1572370608e
zebwebsolutions <rahmanzeb_tdr@msn.com>
2026-01-21 13:42:03 +0500

    adds route for orders

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php 
 
 use App\Http\Controllers\Api\BarcodeScanController;
+use App\Http\Controllers\Api\OrderController;
 
 Route::get('/products/barcode/{barcode}', [BarcodeScanController::class, 'scan']);
+Route::post('/orders', [OrderController::class, 'store']);
