475adcbc10825f29f38490f74c496d0e1d3b850e
zebwebsolutions <rahmanzeb_tdr@msn.com>
2026-02-03 01:15:43 +0500

    adds create product functionality on mobile

---
+++ app/Http/Controllers/Api/BrandController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+
+class BrandController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'data' => Brand::select('id', 'name')
+                ->orderBy('name')
+                ->get()
+        ]);
+    }
+}
